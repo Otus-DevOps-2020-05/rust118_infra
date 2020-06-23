4eeca835ef9adf07cefbd7efb9ddabc0852eded3
elmar <rust@net10.space>
2020-06-24 01:55:17 +0300

    metadata.yaml: universe and multiverse repos added

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y ruby-full ruby-bundler build-essential git
-sudo apt-get install -y  apt-transport-https 
+sudo apt-get install -y ruby-full ruby-bundler build-essential
