99288250332948ddbbfaabfc1a400f4131b95c98
elmar <rust@net10.space>
2020-06-23 23:39:13 +0300

    deploy.sh: remove git installation

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if ![ -x /usr/bin/git ] then
-    sudo apt-get install -y git
-fi
-
 git clone -b monolith https://github.com/express42/reddit.git && cd reddit && bundle install && puma -d
