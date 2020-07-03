0ba68fadc902bcc20272b15a86165b951facce0c
elmar <rust@net10.space>
2020-07-03 13:19:26 +0300

    dynamic target list for load balancer

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,7 @@
-output "external_ip_address_my-app0" {
-  value = yandex_compute_instance.my-app[0].network_interface.0.nat_ip_address
+output "my-app_external_ip_addresses" {
+  value = yandex_compute_instance.my-app[*].network_interface.0.nat_ip_address
 }
 
-output "external_ip_address_my-app1" {
-  value = yandex_compute_instance.my-app[1].network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_my-lb1" {
+output "my-lb_external_ip_address" {
   value = yandex_lb_network_load_balancer.my-app-lb.listener.*.external_address_spec.0.address
 }
