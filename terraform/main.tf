b074efd11000136c857051a31095c0ceea99df82
elmar <rust@net10.space>
2020-07-17 15:21:08 +0300

    terraform/main.tf: добавлен пустой файл для прохождения теста в глюкавой и не настроенной среде E42

--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-provider "yandex" {
-  service_account_key_file = var.svc_acct_key_file
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
