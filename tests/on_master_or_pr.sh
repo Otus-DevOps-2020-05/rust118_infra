cbfabb119f603b849693739863dbc6a6d15463b2
elmar <rust@net10.space>
2020-07-28 00:04:33 +0300

    .travis.yml: reimplement test conditions

--- tests/on_master_or_pr.sh
+++ tests/on_master_or_pr.sh
@@ -5,11 +5,13 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 
+PATH=$TRAVIS_BUILD_DIR:$PATH
+
 cd $TRAVIS_BUILD_DIR
 
-curl -o p.zip https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip && unzip p.zip
-curl -o tf.zip https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip && unzip tf.zip
-curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip && unzip tflint.zip
+curl -o p.zip https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip && unzip -o p.zip
+curl -o tf.zip https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip && unzip -o tf.zip
+curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip && unzip -o tflint.zip
 pip install --user ansible-lint
 #- sudo apt update
 #- sudo apt install software-properties-common
