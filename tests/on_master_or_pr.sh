c83703330d853a8ed7733cb2d0bd61d182a055d8
elmar <rust@net10.space>
2020-07-28 09:02:45 +0300

    .travis.yml: reimplement test conditions

--- tests/on_master_or_pr.sh
+++ tests/on_master_or_pr.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-HOMEWORK=Otus-DevOps-2020-05/rust118_infra
-
+HOMEWORK=/srv/Otus-DevOps-2020-05/rust118_infra
 cd $HOMEWORK
-pwd
 
-if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] || ( [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$TRAVIS_BRANCH" == "master" ] )
-then
-    pwd
+echo "validating db.json"  && packer validate -var-file $HOMEWORK/packer/variables.json packer/db.json
+echo "validating app.json" && packer validate -var-file $HOMEWORK/packer/variables.json packer/app.json
 
-    echo "validating db.json"  && packer validate -var-file $HOMEWORK/packer/variables.json packer/db.json
-    echo "validating app.json" && packer validate -var-file $HOMEWORK/packer/variables.json packer/app.json
+cd $HOMEWORK/terraform/stage
+terraform init && terraform validate && tflint --module --var-file terraform.tfvars.example
 
-    cd $HOMEWORK/terraform/stage
-    terraform init && terraform validate && tflint --module --var-file terraform.tfvars.example
+cd $HOMEWORK/terraform/prod
+terraform init && terraform validate && tflint --module --var-file terraform.tfvars.example
 
-    cd $HOMEWORK/terraform/prod
-    terraform init && terraform validate && tflint --module --var-file terraform.tfvars.example
-
-    cd $HOMEWORK/ansible/playbooks
-    for playbook in ./*.yml; do
-        ansible-lint -v --exclude=roles/jdauphant.nginx $playbook
-    done
-fi
+cd $HOMEWORK/ansible/playbooks
+for playbook in ./*.yml; do
+    ansible-lint -v --exclude=roles/jdauphant.nginx $playbook
+done
--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker exec hw-test bash ./tests/on_master_or_pr.sh
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] || ( [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$TRAVIS_BRANCH" == "master" ] ) then
+    docker exec hw-test bash ./tests/on_master_or_pr.sh
+fi
