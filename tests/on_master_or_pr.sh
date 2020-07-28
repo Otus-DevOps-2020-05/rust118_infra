f4ea971ea56d665fe35638cf46992780b3b6fb84
elmar <rust@net10.space>
2020-07-28 08:31:22 +0300

    .travis.yml: reimplement test conditions

--- tests/on_master_or_pr.sh
+++ tests/on_master_or_pr.sh
@@ -5,9 +5,14 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 
-PATH=$TRAVIS_BUILD_DIR:$PATH
 
-cd $TRAVIS_BUILD_DIR
+
+#PATH=$TRAVIS_BUILD_DIR:$PATH
+HOMEWORK=Otus-DevOps-2020-05/rust118_infra
+
+cd $HOMEWORK
+
+pwd
 
 #curl -o p.zip https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip && unzip -o p.zip
 #curl -o tf.zip https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip && unzip -o tf.zip
@@ -18,16 +23,16 @@ cd $TRAVIS_BUILD_DIR
 #- sudo apt-add-repository --yes --update ppa:ansible/ansible
 #- sudo apt install ansible
 
-echo "validating db.json"  && packer validate -var-file packer/variables.json packer/db.json
-echo "validating app.json" && packer validate -var-file packer/variables.json packer/app.json
+echo "validating db.json"  && packer validate -var-file $HOMEWORK/packer/variables.json packer/db.json
+echo "validating app.json" && packer validate -var-file $HOMEWORK/packer/variables.json packer/app.json
 
-cd $TRAVIS_BUILD_DIR/terraform/stage
+cd $HOMEWORK/terraform/stage
 terraform init && terraform validate && tflint --module --var-file terraform.tfvars.example
 
-cd $TRAVIS_BUILD_DIR/terraform/prod
+cd $HOMEWORK/terraform/prod
 terraform init && terraform validate && tflint --module --var-file terraform.tfvars.example
 
-cd $TRAVIS_BUILD_DIR/ansible/playbooks
+cd $HOMEWORK/ansible/playbooks
 # > /dev/null
 for playbook in ./*.yml; do
     ansible-lint -v --exclude=roles/jdauphant.nginx $playbook
